cb69c9d03a81401fe4b0c5b99ba1537b791208a0
Marco <mbarozzi@gmail.com>
2024-08-07 03:31:18 -0300

    adicionando o hive

--- flutter_listin/lib/main.dart
+++ flutter_listin/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_listin/_core/listin_routes.dart';
 import 'package:flutter_listin/_core/listin_theme.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
